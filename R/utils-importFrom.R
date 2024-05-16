80043826d78107215939bef0594064a4508eeeea
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2024-05-16 17:39:52 +0200

    fixes typo

--- R/utils-importFrom.R
+++ R/utils-importFrom.R
@@ -18,4 +18,4 @@ NULL
 # It is included because otherwise the test-bsafe fails when running inside check
 # It is a known shiny issue that the following error may appear when including markdowns
 # Error in packageVersion: there is no package called ‘markdown’
-invisible(markdown::mark())
+invisible(markdown::mark)
