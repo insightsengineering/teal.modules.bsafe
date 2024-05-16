af4f8289c2ef614a3d2772654bd629ad564ceb9f
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2024-05-16 13:39:42 +0200

    move app creation into test

--- tests/testthat/test-bsafe_app.R
+++ tests/testthat/test-bsafe_app.R
@@ -1,17 +1,17 @@
 # app tests ----
-local({
-  app <- shinytest2::AppDriver$new(teal.modules.bsafe:::mock_bsafe())
-  app$wait_for_idle()
+local({  
   test_that("bsafe module starts", {
+    app <- shinytest2::AppDriver$new(teal.modules.bsafe:::mock_bsafe())
+    app$wait_for_idle()
     checkmate::expect_string(app$get_html("body"), min.chars = 1)
   })
 })
 
 # app tests ----
-local({
-  app <- shinytest2::AppDriver$new(teal.modules.bsafe:::mock_teal())
-  app$wait_for_idle()
+local({  
   test_that("tm_bsafe starts", {
+    app <- suppressWarnings(shinytest2::AppDriver$new(teal.modules.bsafe:::mock_teal()))
+    app$wait_for_idle()
     checkmate::expect_string(app$get_html("body"), min.chars = 1)
   })
 })
