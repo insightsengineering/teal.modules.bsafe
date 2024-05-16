6f61406650c46856351c385fb6576ec4f0750c20
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2024-05-16 12:40:47 +0200

    styling

--- tests/testthat/test-bsafe_app.R
+++ tests/testthat/test-bsafe_app.R
@@ -2,9 +2,9 @@
 local({
   app <- shinytest2::AppDriver$new(teal.modules.bsafe:::mock_bsafe())
   app$wait_for_idle()
-  test_that("bsafe module starts", {    
+  test_that("bsafe module starts", {
     checkmate::expect_string(app$get_html("body"), min.chars = 1)
-  })  
+  })
 })
 
 # app tests ----
