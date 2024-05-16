30b868a7e2baab8fbaf5d3fda91c30a1c9f4a67c
Luis Moris Fernandez <luis.moris.fernandez@gmail.com>
2024-05-16 13:39:58 +0200

    fixes markdown error when running tests during check

--- R/utils-importFrom.R
+++ R/utils-importFrom.R
@@ -13,3 +13,9 @@ NULL
 #' @keywords internal
 #' @importFrom shinymeta ..
 NULL
+
+# This prevents a warning in check.
+# It is included because otherwise the test-bsafe fails when running inside check
+# It is a known shiny issue that the following error may appear when including markdowns
+# Error in packageVersion: there is no package called ‘markdown’
+invisible(markdown::mark())
