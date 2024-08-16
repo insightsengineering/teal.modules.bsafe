insightsengineering/teal.modules.bsafe

commit 6e0c3fdb0f3703c96e1c538146576a50e60aceb5

passes analysis_type to area under the curve
Originated by a change in bsafe

R/mod_decision_making.R
@@ -185,9 +185,9 @@ mod_decision_making_server <- function(
         shinymeta::metaExpr({
           bsafe::area_under_the_curve(
             ae_prop = ..(r_ae_prop),
-            select_analysis = ..(analysis_type()),
             mix = ..(mix()),
-            saf_topic = ..(safety_topic())
+            saf_topic = ..(safety_topic()),
+            select_analysis = ..(analysis_type())
           )
         })
       },
@@ -231,17 +231,19 @@ mod_decision_making_server <- function(
   shiny::moduleServer(id, mod)
 }
 
+# TODO: Alexander input on what to expect and what to put in
 calc_log_hazard_area <- function(param_approx) {
   val <- c(
-    round(RBesT::qmix(param_approx, 0.01), 2),
-    round(RBesT::qmix(param_approx, 0.99), 2)
+    round(param_approx[2, 1] - 2 * param_approx[3, 1], 3),
+    round(param_approx[2, 1] + 2 * param_approx[3, 1], 3)
   )
   return(val)
 }
 
+# TODO: Alexander input on what to expect and what to put in
 calc_param_approx_boundaries <- function(param_approx) {
-  lower_bound <- RBesT::qmix(param_approx, 0.25)
-  upper_bound <- RBesT::qmix(param_approx, 0.75)
+  lower_bound <- param_approx[2, 1] - param_approx[3, 1]
+  upper_bound <- param_approx[2, 1] + param_approx[3, 1]
   return(c(lower_bound, upper_bound))
 }
 

commit bb82324be8182d11cb7dc48bac725df7e942c011

fix bug introduced in
6e0c3fdb0f3703c96e1c538146576a50e60aceb5

Stash was popped an incorrect code was introduced

R/mod_decision_making.R
@@ -231,19 +231,17 @@ mod_decision_making_server <- function(
   shiny::moduleServer(id, mod)
 }
 
-# TODO: Alexander input on what to expect and what to put in
 calc_log_hazard_area <- function(param_approx) {
   val <- c(
-    round(param_approx[2, 1] - 2 * param_approx[3, 1], 3),
-    round(param_approx[2, 1] + 2 * param_approx[3, 1], 3)
+    round(RBesT::qmix(param_approx, 0.01), 2),
+    round(RBesT::qmix(param_approx, 0.99), 2)
   )
   return(val)
 }
 
-# TODO: Alexander input on what to expect and what to put in
 calc_param_approx_boundaries <- function(param_approx) {
-  lower_bound <- param_approx[2, 1] - param_approx[3, 1]
-  upper_bound <- param_approx[2, 1] + param_approx[3, 1]
+  lower_bound <- RBesT::qmix(param_approx, 0.25)
+  upper_bound <- RBesT::qmix(param_approx, 0.75)
   return(c(lower_bound, upper_bound))
 }
 